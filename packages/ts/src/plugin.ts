a77d6624637377997f0d1894317f13d671d35991
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2026-01-13 08:02:25 -0500

    feat(ts): implement arrayFlatMethods rule (#1382)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #814
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/flint-fyi/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/flint-fyi/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Implements the `arrayFlatMethods` rule for the TypeScript plugin. This
    rule reports legacy techniques to flatten arrays and suggests using
    `.flat()` instead.
    
    Flagged patterns include:
    - `array.flatMap(value => value)` (identity flatMap)
    - `[].concat(...array)` (spread into concat)
    - `[].concat.apply([], array)` and `Array.prototype.concat.apply([],
    array)`
    - `Array.prototype.concat.call([], ...array)`
    
    ❤️‍🔥

--- packages/ts/src/plugin.ts
+++ packages/ts/src/plugin.ts
@@ -15,6 +15,7 @@ import arrayExistenceChecksConsistency from "./rules/arrayExistenceChecksConsist
 import arrayFilteredFinds from "./rules/arrayFilteredFinds.ts";
 import arrayFinds from "./rules/arrayFinds.ts";
 import arrayFlatMapMethods from "./rules/arrayFlatMapMethods.ts";
+import arrayFlatMethods from "./rules/arrayFlatMethods.ts";
 import arrayMapIdentities from "./rules/arrayMapIdentities.ts";
 import arrayMutableReverses from "./rules/arrayMutableReverses.ts";
 import arrayMutableSorts from "./rules/arrayMutableSorts.ts";
@@ -123,6 +124,7 @@ export const ts = createPlugin({
 		arrayFilteredFinds,
 		arrayFinds,
 		arrayFlatMapMethods,
+		arrayFlatMethods,
 		arrayMapIdentities,
 		arrayMutableReverses,
 		arrayMutableSorts,
